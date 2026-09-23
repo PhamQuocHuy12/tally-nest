f348251b4cc03766b470debf3e1b88c0260d78ad
PhamQuocHuy12 <huygreen6915@gmail.com>
2026-09-23 23:00:34 +0700

    [HUY] Add esbuild configuration and initial serverless template for TallyNest

--- apps/backend/esbuild.config.mjs
+++ apps/backend/esbuild.config.mjs
@@ -1,4 +1,7 @@
+import { rm } from 'node:fs/promises';
 import { build } from 'esbuild';
+
+await rm('dist', { recursive: true, force: true });
 
 await build({
   bundle: true,
@@ -10,6 +13,9 @@ await build({
   },
   external: ['@aws-sdk/*'],
   format: 'esm',
+  outExtension: {
+    '.js': '.mjs',
+  },
   logLevel: 'info',
   minify: false,
   outdir: 'dist',
